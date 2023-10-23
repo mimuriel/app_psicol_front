559ddcf429be6f04a69d6bb5d30b6ad190cd22b6
DESKTOP-QELL34I\mimuriel <mimv0305@gmail.com>
2023-10-23 06:41:47 -0500

    Se mejora la vista principal

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,13 @@
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <div class="collapse navbar-collapse container" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Inicio</a>
           </li>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Estudiantes
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <router-link class="dropdown-item" to="/viewE">Listar</router-link>
-              </li>
-              <li>
-                <router-link class="dropdown-item" to="/CreateE">Registrar Nuevo</router-link>
-              </li>
-
-            </ul>
-          </li>
-
-          <li class="nav-item dropdown">
+                    <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Profesor
             </a>
@@ -52,6 +37,21 @@
               </li>
               <li>
                 <router-link class="dropdown-item" to="/CreateA">Registrar Nueva</router-link>
+              </li>
+
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Estudiantes
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <router-link class="dropdown-item" to="/viewE">Listar</router-link>
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/CreateE">Registrar Nuevo</router-link>
               </li>
 
             </ul>
